6494c797f3e5925fa196c77c95c5602186564c4a
Sergey_Pimenov <pimenov.web@gmail.com>
2017-05-02 00:30:22 +0300

    tools arrays

---
+++ toolsList.js
@@ -0,0 +1,9 @@
+var toolsArr = [
+	['animate0.js', 'images/snapsvg.png', ['SVG', 'Canvas'], '50', 'http://google.ru'],
+	['animate1.js', 'images/snapsvg.png', ['SVG', 'CSS'], '80', 'http://google.ru'],
+	['animate1.js', 'images/snapsvg.png', ['SVG', 'CSS'], '60', 'http://google.ru'],
+	['animate1.js', 'images/snapsvg.png', ['SVG', 'CSS'], '60', 'http://google.ru'],
+	['animate1.js', 'images/snapsvg.png', ['SVG', 'CSS'], '100', 'http://google.ru'],
+	['animate1.js', 'images/snapsvg.png', ['SVG', 'CSS'], '60', 'http://google.ru'],
+	['animate2.js', 'images/snapsvg.png', ['SVG'],'70', 'http://google.ru']
+];
