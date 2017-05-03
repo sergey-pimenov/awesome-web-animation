a5d940f980006f30f323ea40bd879cbc41077ea2
Sergey Pimenov <pimenov.web@gmail.com>
2017-05-03 20:06:07 +0300

    add new styles

--- __sources/--gulp/gulpfile.js
+++ __sources/--gulp/gulpfile.js
@@ -2,7 +2,9 @@ var gulp = require('gulp');
 var pug = require('gulp-pug');
 var sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
+var autoprefixer = require('gulp-autoprefixer');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 var browserSync = require('browser-sync').create();
 
 gulp.task('pug', function buildHTML() {
@@ -18,6 +20,10 @@ gulp.task('sass', function() {
   return gulp.src('../styles/index.sass')
     .pipe(sass())
     .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(autoprefixer({
+        browsers: ['last 10 versions'],
+        cascade: false
+    }))
     .pipe(gulp.dest('../../styles/'))
     .pipe(browserSync.reload({
       stream: true
@@ -29,6 +35,7 @@ var scripts_modules = ['../scripts/basic.js', '../scripts/filter.js']
 gulp.task('scripts', function() {
   return gulp.src(scripts_modules)
     .pipe(concat('index.js'))
+    .pipe(uglify())
     .pipe(gulp.dest('../../scripts/'))
     .pipe(browserSync.reload({
       stream: true
