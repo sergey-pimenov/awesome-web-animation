a52b0ec8cf2e136c98ef9cd8946e32d494fb39ff
Sergey_Pimenov <pimenov.web@gmail.com>
2017-09-15 02:33:14 +0300

    Add data

---
+++ data/articles.js
@@ -0,0 +1,170 @@
+var articles = [
+	{
+		"name" : "CSS Animation",
+		"id" : "CSSAnimation",
+		"articles" : [
+			{
+				"title" : "Big hisory about development most popular animation library",
+				"date" : "12.07.17",
+				"link" : "https://google.com",
+				"type" : "video"
+			},
+			{
+				"title" : "Big hisory about development most popular animation library",
+				"date" : "12.07.17",
+				"link" : "https://google.com",
+				"type" : "text"
+			},
+			{
+				"title" : "Big hisory about development most popular animation library",
+				"date" : "12.07.17",
+				"link" : "https://google.com",
+				"type" : "text"
+			},
+			{
+				"title" : "Big hisory about development most popular animation library",
+				"date" : "12.07.17",
+				"link" : "https://google.com",
+				"type" : "video"
+			},
+			{
+				"title" : "Big hisory about development most popular animation library",
+				"date" : "12.07.17",
+				"link" : "https://google.com",
+				"type" : "text"
+			},
+			{
+				"title" : "Big hisory about development most popular animation library",
+				"date" : "12.07.17",
+				"link" : "https://google.com",
+				"type" : "text"
+			},
+			{
+				"title" : "Big hisory about development most popular animation library",
+				"date" : "12.07.17",
+				"link" : "https://google.com",
+				"type" : "video"
+			},
+			{
+				"title" : "Big hisory about development most popular animation library",
+				"date" : "12.07.17",
+				"link" : "https://google.com",
+				"type" : "text"
+			},
+			{
+				"title" : "Big hisory about development most popular animation library",
+				"date" : "12.07.17",
+				"link" : "https://google.com",
+				"type" : "text"
+			},
+			{
+				"title" : "Big hisory about development most popular animation library",
+				"date" : "12.07.17",
+				"link" : "https://google.com",
+				"type" : "text"
+			},
+			{
+				"title" : "Big hisory about development most popular animation library",
+				"date" : "12.07.17",
+				"link" : "https://google.com",
+				"type" : "text"
+			},
+			{
+				"title" : "Big hisory about development most popular animation library",
+				"date" : "12.07.17",
+				"link" : "https://google.com",
+				"type" : "text"
+			},
+			{
+				"title" : "Big hisory about development most popular animation library",
+				"date" : "12.07.17",
+				"link" : "https://google.com",
+				"type" : "video"
+			}
+		]
+	},
+	{
+		"name" : "SVG Animation",
+		"id" : "SVGAnimation",
+		"articles" : [
+			{
+				"title" : "Big hisory about development most popular animation library",
+				"date" : "12.07.17",
+				"link" : "https://google.com",
+				"type" : "text"
+			},
+			{
+				"title" : "Big hisory about development most popular animation library",
+				"date" : "12.07.17",
+				"link" : "https://google.com",
+				"type" : "text"
+			},
+			{
+				"title" : "Big hisory about development most popular animation library",
+				"date" : "12.07.17",
+				"link" : "https://google.com",
+				"type" : "text"
+			},
+			{
+				"title" : "Big hisory about development most popular animation library",
+				"date" : "12.07.17",
+				"link" : "https://google.com",
+				"type" : "text"
+			},
+			{
+				"title" : "Big hisory about development most popular animation library",
+				"date" : "12.07.17",
+				"link" : "https://google.com",
+				"type" : "video"
+			},
+			{
+				"title" : "Big hisory about development most popular animation library",
+				"date" : "12.07.17",
+				"link" : "https://google.com",
+				"type" : "text"
+			},
+			{
+				"title" : "Big hisory about development most popular animation library",
+				"date" : "12.07.17",
+				"link" : "https://google.com",
+				"type" : "text"
+			},
+			{
+				"title" : "Big hisory about development most popular animation library",
+				"date" : "12.07.17",
+				"link" : "https://google.com",
+				"type" : "text"
+			},
+			{
+				"title" : "Big hisory about development most popular animation library",
+				"date" : "12.07.17",
+				"link" : "https://google.com",
+				"type" : "text"
+			},
+			{
+				"title" : "Big hisory about development most popular animation library",
+				"date" : "12.07.17",
+				"link" : "https://google.com",
+				"type" : "text"
+			},
+			{
+				"title" : "Big hisory about development most popular animation library",
+				"date" : "12.07.17",
+				"link" : "https://google.com",
+				"type" : "video"
+			},
+			{
+				"title" : "Big hisory about development most popular animation library",
+				"date" : "12.07.17",
+				"link" : "https://google.com",
+				"type" : "text"
+			},
+			{
+				"title" : "Big hisory about development most popular animation library",
+				"date" : "12.07.17",
+				"link" : "https://google.com",
+				"type" : "text"
+			}
+		]
+	}
+];
