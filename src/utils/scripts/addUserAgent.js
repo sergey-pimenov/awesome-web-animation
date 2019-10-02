f10c9e45c0610c96dacd307131dc00a37b8f49ab
sergey-pimenov <pimenov.web@gmail.com>
2019-10-02 20:59:57 +0300

    Clean legacy

--- src/utils/scripts/addUserAgent.js
+++
@@ -1,4 +0,0 @@
-export default function() {
-	var doc = document.documentElement;
-	doc.setAttribute('data-useragent', navigator.userAgent);
-}
--- src/utils/scripts/render.js
+++
@@ -1,3 +0,0 @@
-export default function (element, target) {
-	target.innerHTML += element;
-}
