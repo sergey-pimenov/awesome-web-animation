33414f630da386d999e0fb0ff4dda3eaa78c55c5
sergey-pimenov <pimenov.web@gmail.com>
2019-11-18 23:01:32 +0300

    Test API

--- src/components/item/index.js
+++ src/components/item/index.js
@@ -13,7 +13,8 @@ function Item({name, website, repo, stars}) {
     fetch(`${API}${repo}`, {
       headers: new Headers({
         "Authorization": "token 7706ae46d4b5dd21f2294f11de07633177d796ae",
-        "User-Agent": "sergey-pimenov"
+        "Accept": "application/vnd.github.v3+json",
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 YaBrowser/16.3.0.7146 Yowser/2.5 Safari/537.36"
       }),
     })
       .then(response => response.json())
