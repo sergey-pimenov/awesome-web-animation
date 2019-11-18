7921e9e998478485693ca03fd5717567db71a56e
sergey-pimenov <pimenov.web@gmail.com>
2019-11-18 22:33:38 +0300

    Prototype nav component

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -7,14 +7,30 @@ function Navigation() {
 
   return (
     <nav className={s('navigation')}>
-      <a className={s('item')} href="#SVG"> SVG </a>
-      <a className={s('item')} href="#CSS"> CSS </a>
-      <a className={s('item')} href="#Canvas"> Easing </a>
-      <a className={s('item')} href="#Canvas"> Scroll </a>
-      <a className={s('item')} href="#Canvas"> Canvas </a>
-      <a className={s('item')} href="#Engines"> Engines </a>
-      <a className={s('item')} href="#Engines"> Text </a>
-      <a className={s('item')} href="#Engines"> With GUI </a>
+      <a className={s('item')} href="#SVG">
+        SVG
+      </a>
+      <a className={s('item')} href="#CSS">
+        CSS
+      </a>
+      <a className={s('item')} href="#Canvas">
+        Easing
+      </a>
+      <a className={s('item')} href="#Canvas">
+        Scroll
+      </a>
+      <a className={s('item')} href="#Canvas">
+        Canvas
+      </a>
+      <a className={s('item')} href="#Engines">
+        Engines
+      </a>
+      <a className={s('item')} href="#Engines">
+        Text
+      </a>
+      <a className={s('item')} href="#Engines">
+        With GUI
+      </a>
     </nav>
   );
 }
