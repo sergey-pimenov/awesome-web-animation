14b7f67a948d02e90d84aa886afd0023b4c22913
sergey-pimenov <pimenov.web@gmail.com>
2019-11-22 18:12:37 +0300

    Prettify header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -7,9 +7,11 @@ function Header() {
 
   return (
     <header className={s('header')}>
-      <h1 className={s('appTitle')}> Usefull staff for <br/> <strong>web animation</strong> </h1>
+      <h1 className={s('appTitle')}>
+        Usefull staff for <br /> <strong>web animation</strong>
+      </h1>
     </header>
-  )
+  );
 }
 
 export default Header;
