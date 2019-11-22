8c680102577c4cfa3c3d82206310762e127fecc2
sergey-pimenov <pimenov.web@gmail.com>
2019-11-22 19:06:32 +0300

    Refactor "fetchDataToHook" util func

--- src/utils/scripts/fetchDataToHook.js
+++ src/utils/scripts/fetchDataToHook.js
@@ -3,11 +3,6 @@ export default ({ api, path = '', headers, callback }) => {
     headers: new Headers(headers),
   })
     .then((response) => {
-      if (response.status !== 200) {  
-        console.log('ERRRROR!!!111123123');  
-        return;  
-      }
-
-      response.json().then(data => callback(data));  
+      if (response.status === 200) response.json().then(data => callback(data));
     })
 }
