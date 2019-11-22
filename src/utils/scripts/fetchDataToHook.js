524c0920ecb7b56a53e6793b8af667a56b196b1a
sergey-pimenov <pimenov.web@gmail.com>
2019-11-22 19:08:55 +0300

    Prettify fetchDataToHook func

--- src/utils/scripts/fetchDataToHook.js
+++ src/utils/scripts/fetchDataToHook.js
@@ -2,7 +2,7 @@ export default ({ api, path = '', headers, callback }) => {
   fetch(`${api}${path}`, {
     headers: new Headers(headers),
   })
-    .then((response) => {
+    .then(response => {
       if (response.status === 200) response.json().then(data => callback(data));
-    })
+    });
 }
