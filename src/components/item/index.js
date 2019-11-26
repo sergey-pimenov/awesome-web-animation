dbd26c640d93ceb951a3dbfeccbae4d4694aa4f1
sergey-pimenov <pimenov.web@gmail.com>
2019-11-26 16:58:51 +0300

    Add "LinesEllipsis" lib

--- src/components/item/index.js
+++ src/components/item/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import LinesEllipsis from 'react-lines-ellipsis/lib/loose';
 import endpoint from '../../endpoints.json';
 import fetchDataToHook from '../../utils/scripts/fetchDataToHook';
 import InfoBar from '../infoBar';
@@ -21,33 +22,33 @@ function Item({ repo, bundleData }) {
 
   return (
     <div className={s.item}>
-      {
-        repoData && (
-          <div className={s.itemContent}>
-            <a
-              href={repoData.homepage || repoData.homepage}
-              aria-label={`Link to ${repoData.name}`}
-              className={s.itemLink}
-            > </a>
-            <div className={s.itemHeader}>
-                <img
-                  className={s.repoOwnerLogo}
-                  src={repoData.owner.avatar_url}
-                  alt={repoData.owner.login}
-                />
-              <h3 className={s.name}> {repoData.name} </h3>
-            </div>
-            {repoData.description && (
-              <p className={s.description}> {repoData.description} </p>
-            )}
-            <InfoBar
-              repoData={repoData}
-              bundleData={bundleData}
-              repo={repo}
+      {repoData && (
+        <div className={s.itemContent}>
+          <a
+            href={repoData.homepage || repoData.html_url}
+            aria-label={`Link to ${repoData.name}`}
+            className={s.itemLink}
+          > </a>
+          <div className={s.itemHeader}>
+            <img
+              className={s.repoOwnerLogo}
+              src={repoData.owner.avatar_url}
+              alt={repoData.owner.login}
             />
+            <h3 className={s.name}> {repoData.name} </h3>
           </div>
-        )
-      }
+          {repoData.description && (
+            <LinesEllipsis
+              text={repoData.description}
+              maxLine="3"
+              lineHeight="20"
+              component="p"
+              className={s.description}
+            />
+          )}
+          <InfoBar repoData={repoData} bundleData={bundleData} repo={repo} />
+        </div>
+      )}
     </div>
   );
 }
