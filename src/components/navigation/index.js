6436a8855ffa6ebd87aa09a358d1391e379d0311
sergey-pimenov <pimenov.web@gmail.com>
2019-12-06 02:35:42 +0300

    Change names

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -4,11 +4,14 @@ import s from './navigation.css';
 function Navigation() {
   return (
     <nav className={s.navigation}>
+      <a className={s.item} href="#Books">
+        Books
+      </a>
       <a className={s.item} href="#SVG">
         SVG
       </a>
-      <a className={s.item} href="#CSS">
-        CSS
+      <a className={s.item} href="#Common">
+        Common
       </a>
       <a className={s.item} href="#Canvas">
         Easing
@@ -19,8 +22,8 @@ function Navigation() {
       <a className={s.item} href="#Canvas">
         Canvas
       </a>
-      <a className={s.item} href="#Engines">
-        Engines
+      <a className={s.item} href="#CSS">
+        CSS
       </a>
       <a className={s.item} href="#Engines">
         Text
