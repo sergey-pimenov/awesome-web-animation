ffcf4797958b8fe645862b110324147fff27da99
sergey-pimenov <pimenov.web@gmail.com>
2019-12-09 16:00:27 +0300

    Add big heart img

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -5,12 +5,12 @@ import love from './img/love.svg';
 function Footer() {
   return (
     <footer className={s.footer}>
-      <div className={s.lineOne}>Made with</div>
-      <div className={s.lineTwo}>love</div>
-      <img className={s.love} src={love} alt="Love" />
-      <div className={s.lineThree}>
-        <a href="https://github.com/sergey-pimenov/" target="_blank" rel="noopener noreferrer">by Sergey Pimenov</a>
-      </div>
+      <a href="https://github.com/sergey-pimenov/" target="_blank" rel="noopener noreferrer">
+        <div className={s.lineOne}>Made with</div>
+        <div className={s.lineTwo}>love</div>
+        <img className={s.love} src={love} alt="Love" />
+        <div className={s.lineThree}> by Sergey Pimenov </div>
+      </a>
     </footer>
   );
 }
